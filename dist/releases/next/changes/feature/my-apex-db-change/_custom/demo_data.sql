5986ada6a66e5061ddc5dce1baba39713f5e3ac7
noorul-123 <ansari.noorulhoda123@gmail.com>
2026-09-21 14:51:12 +0530

    Add DEMO table data

---
+++ dist/releases/next/changes/feature/my-apex-db-change/_custom/demo_data.sql
@@ -0,0 +1,5 @@
+REM INSERTING into DEMO
+SET DEFINE OFF;
+Insert into DEMO (ID,NAME,LOCATION,CI_CD_TEST_COLUMN,EMAIL,ADDRESS,SALARY) values (2,'Noorul','Kolkata',null,'ansari.noorulhoda123@gmail.com',null,40000);
+Insert into DEMO (ID,NAME,LOCATION,CI_CD_TEST_COLUMN,EMAIL,ADDRESS,SALARY) values (3,'Rehan','Pune',null,'ansari.noorulhoda123@gmail.com',null,30000);
+
