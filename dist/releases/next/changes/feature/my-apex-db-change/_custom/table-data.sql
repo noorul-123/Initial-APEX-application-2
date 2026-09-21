65cb28812fa25bb32ad48a320c71e628f1000c02
noorul-123 <ansari.noorulhoda123@gmail.com>
2026-09-21 16:38:05 +0530

    Export table data

--- dist/releases/next/changes/feature/my-apex-db-change/_custom/table-data.sql
+++ dist/releases/next/changes/feature/my-apex-db-change/_custom/table-data.sql
@@ -6,11 +6,12 @@
 
 
 -- ==============================================
+
+-- ==============================================
 -- TABLE DATA EXPORT
 -- Branch: feature/my-apex-db-change
 -- ==============================================
 
-REM INSERTING into DEMO
+REM INSERTING into CICD_MIGRATIONS
 
-Insert into DEMO (ID,NAME,LOCATION,CI_CD_TEST_COLUMN,EMAIL,ADDRESS,SALARY) values (2,'Noorul','Kolkata',null,'ansari.noorulhoda123@gmail.com',null,40000);
-Insert into DEMO (ID,NAME,LOCATION,CI_CD_TEST_COLUMN,EMAIL,ADDRESS,SALARY) values (3,'Rehan','Pune',null,'ansari.noorulhoda123@gmail.com',null,30000);
+Insert into CICD_MIGRATIONS (VERSION,SCRIPT_NAME,EXECUTED_ON,EXECUTED_BY) values ('v1','CICD',to_timestamp('21-09-26 12:00:00.000000000 AM','DD-MM-RR fmHH12:fmMI:SSXFF AM'),'Noorul');
